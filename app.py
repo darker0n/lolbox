ca75b80c52d063936572685c67edc9b8705c3a7d
darker0n <dark.crak@ya.ru>
2016-05-05 11:41:00 -0400

    Bootstrap design. Error pages. Index page. Exam timer. New font.

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import *
+from flask.ext.bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+        return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+            return render_template('500.html'), 500
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def homepage():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
