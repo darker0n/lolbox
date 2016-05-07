087cfa5aaf3722389ce10acf978d80f1b111b9ed
darker0n <dark.crak@ya.ru>
2016-05-07 18:19:55 -0400

    Remove Bootstrap package. Add Bootstrap into static. Add jQuery. Remove WTF Forms. Refactoring. Change name of app. Turn off debug mode on server. Max symbol filter. Meme on main page :)

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 import os
 from flask import Flask, render_template, redirect, url_for, flash, request
-from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from wtforms import TextAreaField, SubmitField
 from wtforms.validators import *
@@ -10,16 +9,13 @@ import collections
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pashaandmisha'
-bootstrap = Bootstrap(app)
 
 MONGODB_URL = os.environ.get("MONGODB_URI")
 client = pymongo.MongoClient(MONGODB_URL)
-
 db = client.get_default_database()
 
-class NewAnswerForm(Form):
-        answer = TextAreaField(u'напиши сюда свой ответ, чтобы его увидели другие.', validators=[Required()])
-        submit = SubmitField(u'отправить.')
+def get_coll(coll):
+        return db[coll]
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -29,26 +25,23 @@ def page_not_found(e):
 def internal_server_error(e):
         return render_template('500.html'), 500
 
-@app.route('/ticket/<int:number>', methods=['GET', 'POST'])
+@app.route('/topic/<int:number>', methods=['GET', 'POST'])
 def ticket(number):
-        form = NewAnswerForm()
-        collection = db['tickets']
-        ticket = collection.find_one({"number": number})
+        coll = get_coll("topics")
+        topic = coll.find_one({"number": number})
 
-        if request.method == 'POST' and form.validate():
-                result = collection.update_one({"number": number}, {"$pushAll": {"answers": [form.answer.data]}})
+        if request.method == 'POST':
+                result = coll.update_one({"number": number},
+                                               {"$pushAll":
+                                                {"comments": [request.form["text"]]}})
                 flash(u'спс. да хранит тебя Господь.')
-                return redirect('/ticket/' + str(number))
-        return render_template('ticket.html', ticket=ticket, form=form)
+                return redirect(request.path)
+        return render_template('topic.html', topic=topic)
 
 @app.route('/')
 def homepage():
-        collection = db['tickets']
-        tickets = []
-        for ticket in collection.find():
-                tickets.append(ticket)
-        return render_template('index.html', tickets=sorted(tickets, key=lambda k: k["number"]))
+        return render_template('index.html', topics=[x for x in get_coll('topics').find()])
 
 if __name__ == '__main__':
         port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port, debug=True)
+        app.run(host='0.0.0.0', port=port)
